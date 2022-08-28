c5102a57718ec571998ef785883c3df344f0473e
Greed058 <deekshit.bti@gmail.com>
2022-08-29 02:41:43 +0545

    Added spotify icon

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -10,18 +10,18 @@ from .keys import mod, keys
 
 
 # Get the icons at https://www.nerdfonts.com/cheat-sheet (you need a Nerd Font)
-# Icons: 
-# nf-fa-firefox, 
-# nf-fae-python, 
-# nf-dev-terminal, 
-# nf-fa-code, 
-# nf-oct-git_merge, 
+# Icons:
+# nf-fa-firefox,
+# nf-fae-python,
+# nf-dev-terminal,
+# nf-fa-code,
+# nf-oct-git_merge,
 # nf-linux-docker,
-# nf-mdi-image, 
+# nf-mdi-image,
 # nf-mdi-layers
 
 groups = [Group(i) for i in [
-    "   ", "   ", "   ", "   ", "  ", "   ", "   ", "   ", "   ",
+    "   ", "   ", "   ", "   ", "  ", "  ", "   ", "   ", "   ",
 ]]
 
 for i, group in enumerate(groups):
