3a576b4aa939e4b706c0b8676bb4c14ccc35d3c7
Greed058 <deekshit.bti@gmail.com>
2022-08-29 18:16:01 +0545

    More keybindings

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -45,7 +45,7 @@ keys = [Key(key[0], key[1], *key[2:]) for key in [
     ([mod, "control"], "r", lazy.restart()),
 
     ([mod, "control"], "q", lazy.shutdown()),
-    ([mod], "r", lazy.spawncmd()),
+    # ([mod], "r", lazy.spawncmd()),
 
     # ------------ App Configs ------------
 
