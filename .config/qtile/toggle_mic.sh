a835ff4c0efccabbfca1578ad7dda81eca329a6c
Greed058 <deekshit.bti@gmail.com>
2022-09-02 02:15:07 +0545

    Changed Directory for icons

--- .config/qtile/toggle_mic.sh
+++ .config/qtile/toggle_mic.sh
@@ -5,9 +5,9 @@ state=`amixer -D pulse sset Capture toggle | gawk 'match($0, /Front Left.*\[(.*)
 micStatus=""
 if [ $state = "off" ]; then
   micStatus="OFF"
-  icon="~/.dotfiles/icons/microphone-disabled-symbolic.svg"
+  icon="~/icons/microphone-disabled-symbolic.svg"
 else
   micStatus="ON"
-  icon="~/.dotfiles/icons/audio-input-microphone-symbolic.svg"
+  icon="~/icons/audio-input-microphone-symbolic.svg"
 fi
 notify-send --hint=int:transient:1 -t 800 -i $icon "MIC STATUS : $micStatus"
