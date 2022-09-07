bc46fdf620e8afa2def7fbeb9de0ee9b7ef75524
Greed058 <deekshit.bti@gmail.com>
2022-09-08 02:59:38 +0545

    Fish added to dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,44 @@
+# Bang Bang command
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+bind ! __history_previous_command
+bind '$' __history_previous_command_arguments
+
+# set up the same key bindings for insert mode if using fish_vi_key_bindings
+if test "$fish_key_bindings" = 'fish_vi_key_bindings'
+    bind --mode insert ! __history_previous_command
+    bind --mode insert '$' __history_previous_command_arguments
+end
+
+function _plugin-bang-bang_uninstall --on-event plugin-bang-bang_uninstall
+    bind --erase --all !
+    bind --erase --all '$'
+    functions --erase _plugin-bang-bang_uninstall
+end
+#End of bang bang
+
+alias ls "exa -a --icons"
+alias ll "exa -lah --icons"
+alias wl "nmcli device wifi list"
+alias cw "echo 'nmcli device wifi connect SSID password PW'"
+alias sw "nmcli device wifi show"
+
+neofetch --config ~/dotfiles/neofetch/config2.conf
+
+starship init fish | source
