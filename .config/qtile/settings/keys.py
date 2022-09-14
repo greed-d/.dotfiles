713293fbc0cf8996f76d9d424834282653e30351
Greed058 <deekshit.bti@gmail.com>
2022-09-14 20:35:42 +0545

    Added Mic toggle and screenshot script

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -1,3 +1,5 @@
+import os
+
 from libqtile.config import Key
 from libqtile.command import lazy
 from libqtile.core import manager
@@ -77,11 +79,11 @@ keys = [Key(key[0], key[1], *key[2:]) for key in [
     ([mod, "shift"], "r", lazy.spawn("redshift -x")),
 
     # Screenshot
-    ([mod], "s", lazy.spawn("scrot")),
-    ([mod, "shift"], "s", lazy.spawn("scrot -s")),
+    ([mod], "s", lazy.spawn(os.path.expanduser("~/.dotfiles/scripts/scrotshot.sh"))),
+    ([mod, "shift"], "s", lazy.spawn("/home/greed/.dotfiles/scripts/scrot_select.sh")),
 
     # mute mic
-    ([mod], "m", lazy.spawn("/home/greed/.dotfiles/.config/qtile/mic_toggle.sh")),
+    ([mod], "m", lazy.spawn("/home/greed/.dotfiles/scripts/mic_toggle.sh")),
 
 
     # ------------ Hardware Configs ------------
