aeed8e1a19334503ec65894793c2295f0608cafc
Greed058 <deekshit.bti@gmail.com>
2022-10-16 11:09:50 +0545

    feat : Stow Implementaion/Folder Reconfig

---
+++ nvim/.config/nvim/after/plugin/presence.lua
@@ -0,0 +1,9 @@
+require("presence"):setup({
+	neovim_image_text = "Neovim",
+	presence_log_level = "error",
+	presence_editing_text = "Editing « %s »",
+	presence_file_explorer_text = "Browsing files",
+	presence_reading_text = "Reading  « %s »",
+	presence_workspace_text = "Working on « %s »",
+})
+
---
+++ nvim/.config/nvim/lua/greed/init.lua
@@ -0,0 +1,4 @@
+require("greed.packer")
+require("greed.set")
+require("greed.keymaps")
+require("greed.tree")
