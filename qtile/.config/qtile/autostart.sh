98a9b12b2cea9199dc601b9a352358995eb8ba13
Greed058 <deekshit.bti@gmail.com>
2022-10-24 22:37:41 +0545

    refac[startup] : volctl added instead of volumeicon

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -4,12 +4,8 @@ picom --experimental-backends --config ~/.config/picom/picom.conf &
 # systray battery icon
 cbatticon -u 5 &
 # systray volume
-volumeicon &
+volctl &
 #nitrogen
 nitrogen --restore &
 #Discord
 armcord & 
-#freedownloadmanager
-# fdm &
-#for Wifi
-# sudo systemctl start iwd
