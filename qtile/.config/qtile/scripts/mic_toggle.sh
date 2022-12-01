66d2c0fffd94ec1f2f75c059e6460ff966f36359
GRR3ED <deekshit.bti@gmail.com>
2022-12-01 19:00:18 +0545

    refac : icon path changed

--- qtile/.config/qtile/scripts/mic_toggle.sh
+++ qtile/.config/qtile/scripts/mic_toggle.sh
@@ -5,12 +5,13 @@ state=`pacmd list-sources | awk '/muted/ { print $2 }' | tail -1`
 micStatus="no"
 if [ $state = "yes" ]; then
     micStatus="on"
-    icon="/usr/share/icons/Vimix-Black/16/panel/audio-input-microphone-high.svg"
+    icon="/usr/share/icons/Vimix-Black/24/panel/audio-input-microphone-high.svg"
 else
     micStatus="off"
-    icon="/usr/share/icons/Vimix-Black/16/panel/audio-input-microphone-muted.svg"
+    icon="/usr/share/icons/Vimix-Black/24/panel/audio-input-microphone-muted.svg"
 fi
 pactl set-source-mute 1 toggle
 notify-send --hint=int:transient:1 -t 1200 -i $icon "Mic switched: $state"
 #dunstify -h int:transient:1 -t 1000 -i $icon "Mic switched: $state"
 # ~/Scripts/notify.sh "Mic status" "Mic switched: $micStatus" "/usr/share/icons/Adwaita/32x32/$icon.symbolic.png" 2 update
+
