a58f3f2594e406a863895aa34619fe1273a2d059
GRR3ED <deekshit.bti@gmail.com>
2022-12-07 19:14:57 +0545

    refac :  formatter changed to `black`

--- nvim/.config/nvim/after/plugin/lsp-config/null-ls.lua
+++ nvim/.config/nvim/after/plugin/lsp-config/null-ls.lua
@@ -3,7 +3,7 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.formatting.eslint_d,
-		null_ls.builtins.formatting.autopep8,
+		null_ls.builtins.formatting.black,
 		--null_ls.builtins.formatting.gofmt,
 		null_ls.builtins.formatting.prettierd,
 		-- null_ls.builtins.formatting.rustfmt,
