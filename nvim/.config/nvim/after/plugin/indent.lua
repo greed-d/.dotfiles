5b823216208e0c7ccfa0f01d20bb2dd9ee8d5489
GRR3ED <deekshit.bti@gmail.com>
2022-12-23 17:34:11 +0545

    add : `indent-blankline` added

--- nvim/.config/nvim/after/plugin/indent.lua
+++ nvim/.config/nvim/after/plugin/indent.lua
@@ -1,4 +1,5 @@
-require("indent_blankline").setup {
-	show_end_of_line = true,
+require("indent_blankline").setup({
 	space_char_blankline = " ",
-}
+	show_current_context = true,
+	show_current_context_start = true,
+})
