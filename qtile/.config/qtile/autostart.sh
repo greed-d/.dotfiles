b90002492aee99ea8e4cc06cfd3f986d64c103f6
GRR3ED <deekshit.bti@gmail.com>
2023-04-11 19:21:14 +0545

    refac(qtile) : qtile refactored

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Blurring
-picom --experimental-backends --config ~/.config/picom/picom.conf.qtile &
+# picom --experimental-backends --config ~/.config/picom/picom.conf.qtile &
 
 # systray battery icon
 cbatticon -u 5 &
