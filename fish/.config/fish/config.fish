3dab00a7cb49276b9261a139ba650f66b9e6c0aa
GRR3ED <deekshit.bti@gmail.com>
2023-04-11 19:19:13 +0545

    add(func) : config switcher for neovim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -34,16 +34,46 @@ function _plugin-bang-bang_uninstall --on-event plugin-bang-bang_uninstall
 end
 #End of bang bang
 
+function fish_greeting
+    echo Hello $USER!
+    echo You logged in to $hostname at (set_color yellow; date +%T; set_color normal)
+end
+
+function nvdef
+    env NVIM_APPNAME=nvim.bak nvim
+end
+
+function astronvim
+    env NVIM_APPNAME=astronvim nvim
+end
+
+function nvchad
+    env NVIM_APPNAME=nvchad nvim
+end
+
+function nvims
+    set items nvdef astronvim nvchad
+    set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+    if [ -z $config ]
+        echo "Nothing selected"
+        return 0
+    else if [ $config = "default" ]
+        set config ""
+    end
+    env NVIM_APPNAME=$config nvim $argv
+end
+
+bind \ca 'nvims\n'```
 
 alias ls "exa -a --icons --group-directories-first"
-alias ll "exa -lah --color automatic --git --icons --group-directories-first --no-user" 
+alias ll "exa -lah --color automatic -T --git --icons --group-directories-first --no-user" 
 alias wl "nmcli device wifi list"
 #alias cw "echo 'nmcli device wifi connect SSID password PW'"
 alias sw "nmcli device wifi show"
 alias .. "cd .."
 alias ... "cd ../.."
 alias .... "cd ../../.."
-alias e "thunar ."
+alias e "nautilus ."
 alias x "exit"
 alias lg "lazygit"
 alias tns "tmux new -s"
@@ -54,20 +84,15 @@ alias rm "trash"
 alias clr "clear && neofetch --config ~/.config/neofetch/config.small.conf --ascii_distro arch_small"
 alias cc "qtile cmd-obj -o widget wifiicon -f eval -a 'self.is_connected' && qtile cmd-obj -o widget wifiicon -f eval -a 'self.check_connection()'"
 alias ttc "tty-clock -SsctC5"
+alias dot "tmux-sessionizer ~/.dotfiles"
+alias vi "NVIM_APPNAME=nvchad nvim"
 
-#bind \cm "finder"
+bind \cb "tmux-sessionizer"
 
-set PATH "$PATH":"$HOME/.local/share/scripts/"
-set PATH "$PATH":"/opt/flutter/bin/flutter/"
-set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk
-set PATH "$JAVA_HOME/bin:$PATH"
-
+set PATH "$PATH":"$HOME/.local/scripts/"
+set PATH "$PATH":"/opt/flutter/bin"
 neofetch --config ~/.config/neofetch/config.small.conf --ascii_distro arch_small
 
-function fish_greeting
-    echo Hello $USER!
-    echo You logged in to $hostname at (set_color yellow; date +%T; set_color normal)
-end
 
 starship init fish | source
 
