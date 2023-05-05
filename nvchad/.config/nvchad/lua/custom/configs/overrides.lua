95096b1f84b72f52e030ae66b321c48581e02cf4
GRR3ED <deekshit.bti@gmail.com>
2023-05-05 15:01:47 +0545

    `add` : override added for `NvTerm`

--- nvchad/.config/nvchad/lua/custom/configs/overrides.lua
+++ nvchad/.config/nvchad/lua/custom/configs/overrides.lua
@@ -1,101 +1,120 @@
 local M = {}
 
 M.treesitter = {
-	ensure_installed = {
-		"vim",
-		"lua",
-		"html",
-		"css",
-		"javascript",
-		"typescript",
-		"tsx",
-		"c",
-		"markdown",
-		"markdown_inline",
-		"bash",
-		"fish",
-	},
-	indent = {
-		enable = true,
-		-- disable = {
-		--   "python"
-		-- },
-	},
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "bash",
+    "fish",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
 }
 
 M.mason = {
-	ensure_installed = {
-		-- lua stuff
-		"lua-language-server",
-		"stylua",
-		"selene",
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+    "selene",
 
-		-- web dev stuff
-		"css-lsp",
-		"html-lsp",
-		"typescript-language-server",
-		"deno",
-		"prettier",
-		"eslint-lsp",
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+    "eslint-lsp",
 
-		-- shell
-		"shfmt",
-		"shellcheck",
-		"codespell",
-		"bash-language-server",
+    -- shell
+    "shfmt",
+    "shellcheck",
+    "codespell",
+    "bash-language-server",
 
-		-- python
-		"flake8",
-		"python-lsp-server",
-		"autopep8",
-		"black",
+    -- python
+    "flake8",
+    "python-lsp-server",
+    "autopep8",
+    "black",
 
-		-- pretty
-		"prettier",
-		"prettierd",
+    -- pretty
+    "prettier",
+    "prettierd",
 
-		-- c/cpp stuff
-		"clangd",
-		"clang-format",
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
 
-		--markdown
-		"marksman",
-	},
+    --markdown
+    "marksman",
+  },
 }
 -- git support in nvimtree
 M.nvimtree = {
-	git = {
-		enable = true,
-	},
+  git = {
+    enable = true,
+  },
 
-	renderer = {
-		highlight_git = true,
-		icons = {
-			show = {
-				git = true,
-			},
-		},
-	},
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
 }
 
 M.telescope = {
-	opts = {
-		default = {
-			file_ignore_pattern = { ".git", "wallpapers", "wally", "Wallpapers" },
-		},
-		extension_list = { "notify", "flutter" },
-	},
+  opts = {
+    default = {
+      file_ignore_pattern = { ".git" },
+    },
+    extension_list = { "notify", "flutter" },
+  },
+}
+
+M.nvterm = {
+  terminals = {
+    shell = vim.o.shell,
+    list = {},
+    type_opts = {
+      float = {
+        relative = "editor",
+        row = 0.16,
+        col = 0.09,
+        width = 0.75,
+        height = 0.7,
+        border = "single",
+      },
+      horizontal = { location = "rightbelow", split_ratio = 0.3 },
+      vertical = { location = "rightbelow", split_ratio = 0.5 },
+    },
+  },
 }
 
 M.gitsigns = {
-	signs = {
-		add = { hl = "DiffAdd", text = "", numhl = "GitSignsAddNr" },
-		change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
-		delete = { hl = "DiffDelete", text = "", numhl = "GitSignsDeleteNr" },
-		topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-		changedelete = { hl = "DiffChangeDelete", text = "~", numhl = "GitSignsChangeNr" },
-		untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-	},
+  signs = {
+    add = { hl = "DiffAdd", text = "", numhl = "GitSignsAddNr" },
+    change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
+    delete = { hl = "DiffDelete", text = "", numhl = "GitSignsDeleteNr" },
+    topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
+    changedelete = { hl = "DiffChangeDelete", text = "~", numhl = "GitSignsChangeNr" },
+    untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+  },
 }
 
 return M
