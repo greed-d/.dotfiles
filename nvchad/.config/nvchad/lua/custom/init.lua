01233e2a69b6ddbbb1b10e08406c6070ee15054b
GRR3ED <deekshit.bti@gmail.com>
2023-05-05 15:02:09 +0545

    `rem` : removed `lang_maps` and moved to `mappings.lua`

--- nvchad/.config/nvchad/lua/custom/init.lua
+++ nvchad/.config/nvchad/lua/custom/init.lua
@@ -8,55 +8,38 @@ local opt = vim.opt
 
 opt.rnu = true
 
--------------------------------------- language-maps ------------------------------------------
-
-local lang_maps = {
-	cpp = { build = "g++ % -o %:r", exec = "./%:r" },
-	c = { build = "g++ % -o %:r", exec = "./%:r" },
-	typescript = { build = "deno compile %", exec = "deno run %" },
-	javascript = { build = "deno compile %", exec = "deno run %" },
-	python = { exec = "python %" },
-	java = { build = "javac %", exec = "java %:r" },
-	sh = { exec = "./%" },
-	prolog = { exec = "gplc %" },
-}
-
-for lang, cmd in pairs(lang_maps) do
-	if cmd.build ~= nil then
-		autocmd("FileType", { command = "nnoremap <Leader>bu :!" .. cmd.build .. "<CR>", pattern = lang })
-	end
-	autocmd(
-		"FileType",
-		{ command = "nnoremap <Leader>e :90vsplit<CR>:terminal " .. cmd.exec .. "<CR>", pattern = lang }
-	)
-end
-
 -------------------------------------- highlight on yanked ------------------------------------------
 
 autocmd("TextYankPost", {
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({
-			higroup = "IncSearch",
-			timeout = 100,
-		})
-	end,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 100,
+    })
+  end,
 })
 
 -------------------------------------- format on save ------------------------------------------
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	command = "lua vim.lsp.buf.format()",
-	pattern = "*.cpp,*.css,*.go,*.h,*.html,*.js,*.json,*.jsx,*.lua,*.md,*.py,*.rs,*.ts,*.tsx,*.yaml,*.c,*.dart",
+  command = "lua vim.lsp.buf.format()",
+  pattern = "*.cpp,*.css,*.go,*.h,*.html,*.js,*.json,*.jsx,*.lua,*.md,*.py,*.rs,*.ts,*.tsx,*.yaml,*.c,*.dart",
 })
 
 --vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
 --vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
---
+
+-------------------------------------- usercommands -------------------------------------------------
+
 vim.api.nvim_create_user_command("PeekOpen", function()
-	require("peek").open()
+  require("peek").open()
 end, {})
 
 vim.api.nvim_create_user_command("PeekClose", function()
-	require("peek").close()
+  require("peek").close()
+end, {})
+
+vim.api.nvim_create_user_command("Nvtfloat", function()
+  require("nvterm.terminal").toggle("float")
 end, {})
