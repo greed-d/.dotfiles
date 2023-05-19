1e2db4af2ec18df3b25b9d4b763a0f2d5bcf9c02
GRR3ED <deekshit.bti@gmail.com>
2023-05-19 15:53:43 +0545

    `add` : script to remove bak

---
+++ rm_bak.sh
@@ -0,0 +1,15 @@
+mv ~/.config/bspwm.bak ~/.config/bspwm
+mv ~/.config/dunst.bak ~/.config/dunst
+mv ~/.config/fish.bak ~/.config/fish
+mv ~/.config/hypr.bak ~/.config/hypr
+mv ~/.config/mpd.bak ~/.config/mpd
+mv ~/.config/ncmpcpp.bak ~/.config/ncmpcpp
+mv ~/.config/neofetch.bak ~/.config/neofetch
+mv ~/.config/picom.bak ~/.config/picom
+mv ~/.config/polybar.bak ~/.config/polybar
+mv ~/.config/qtile.bak ~/.config/qtile
+mv ~/.config/rofi.bak ~/.config/rofi
+mv ~/.config/sxhkd.bak ~/.config/sxhkd
+mv ~/.config/waybar.bak ~/.config/waybar
+mv ~/.config/starship.toml.bak ~/.config/starship.toml
+
