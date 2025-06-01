66b038681c208995eca48b955159e5204eb314fb
greed <greed-d>
2025-06-01 14:55:31 +0545

    `chore` : minimal changes

---
+++ niri/.config/niri/scripts/autostart.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# waybar --config ~/.config/waybar/configs/\[TOP\]\ My\ Style --style ~/.config/waybar/style/\[Mine\]\ My\ Style.css
+waybar
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
