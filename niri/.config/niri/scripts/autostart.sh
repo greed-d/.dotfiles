26f2a09462cdfc8f662b71e19ec021af722d1683
greed <greed-d>
2025-06-01 14:57:15 +0545

    `chore` : remove startup apps

--- niri/.config/niri/scripts/autostart.sh
+++ niri/.config/niri/scripts/autostart.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 # waybar --config ~/.config/waybar/configs/\[TOP\]\ My\ Style --style ~/.config/waybar/style/\[Mine\]\ My\ Style.css
-waybar
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
